be77aa1a1dce4a1ed727be47ff8b2fca83ac24fa
Samarpan <samarpanrana95@gmail.com>
2025-01-13 22:43:18 +0545

    Add js logic to rps
    Added javascript game logic matching with the ui
    to make the game playable when clicked as well as
    the option to restart the game once it is over

--- javascript.js
+++ javascript.js
@@ -1,15 +1,22 @@
+// Selecting the dom choices to ensure round is played when clicked
+let choicesDom = document.querySelectorAll(".choice");
+for (let choiceDom of choicesDom) {
+  choiceDom.addEventListener(
+    "click",
+    (e) => {
+      e.stopPropagation();
+      playRound(e);
+      if (checkGameOver()) {
+        showModal(getGameWinner());
+      }
+    },
+    { capture: true }
+  );
+}
+
 let choices = ["rock", "paper", "scissors"],
   computerScore = 0,
-  humanScore = 0,
-  totalRounds = 3;
-
-for (let roundNumber = 1; roundNumber <= totalRounds; roundNumber++) {
-  console.log('\n\n');
-  playRound();
-  if (checkGameOver(roundNumber)) {
-    console.log(`${getGameWinner()} won the game !!`);
-  }
-}
+  humanScore = 0;
 
 function getComputerChoice() {
   let randomNumber = Math.floor(Math.random() * 100);
@@ -22,40 +29,21 @@ function getComputerChoice() {
   return computerChoice;
 }
 
-function getHumanChoice() {
+function getHumanChoice(event) {
   let humanChoice;
-  let isInvalid = true;
-  do {
-    humanChoice = prompt("Pick one: rock or paper or scissors");
-    if (humanChoice == null || humanChoice.length <= 0) {
-      alert("Cannot be empty");
-    } else {
-      humanChoice = humanChoice.toLowerCase();
-      switch (humanChoice) {
-        case "rock":
-        case "paper":
-        case "scissors":
-          isInvalid = false;
-          break;
-        default:
-          isInvalid = true;
-          break;
-      }
-      if (isInvalid) {
-        alert("Please input correct option");
-      }
-    }
-  } while (
-    humanChoice == "" ||
-    humanChoice === null ||
-    humanChoice === undefined ||
-    isInvalid
-  );
+  let targetClassList = event.target.classList;
+  if (targetClassList.contains("choice-rock")) {
+    humanChoice = "rock";
+  } else if (targetClassList.contains("choice-paper")) {
+    humanChoice = "paper";
+  } else if (targetClassList.contains("choice-scissors")) {
+    humanChoice = "scissors";
+  }
   return humanChoice;
 }
 
-function playRound() {
-  let humanChoice = getHumanChoice();
+function playRound(event) {
+  let humanChoice = getHumanChoice(event);
   let computerChoice = getComputerChoice();
   let humanWon = false,
     computerWon = false;
@@ -95,37 +83,111 @@ function playRound() {
       alert("That's unusual. Something went wrong");
     }
   }
+  updateDom();
 
-  console.log(
-    `Human played ${humanChoice} and Computer played ${computerChoice}.`
-  );
-  if (humanWon) {
-    console.log("Human won the round.");
-  } else if (computerWon) {
-    console.log("Computer won the round.");
-  } else {
-    console.log("It's a tie!");
+  function updateDom() {
+    let mainText = document.querySelector(".main-text");
+    let subText = document.querySelector(".main-sub-text");
+    let mainHumanImg = document.querySelector(".main-human-img");
+    let mainComputerImg = document.querySelector(".main-computer-img");
+    let mainHumanText = document.querySelector(".main-human-text");
+    let mainComputerText = document.querySelector(".main-computer-text");
+
+    if (humanWon) {
+      mainText.textContent = "Human won the round.";
+    } else if (computerWon) {
+      mainText.textContent = "Computer won the round.";
+    } else {
+      mainText.textContent = "It's a tie!";
+    }
+    subText.textContent = `Human played ${humanChoice} and Computer played ${computerChoice}.`;
+
+    switch (humanChoice) {
+      case "rock":
+        mainHumanImg.textContent = "✊";
+        break;
+      case "paper":
+        mainHumanImg.textContent = "✋";
+        break;
+      case "scissors":
+        mainHumanImg.textContent = "✌";
+        break;
+      default:
+        alert("Something is wrong.");
+    }
+    switch (computerChoice) {
+      case "rock":
+        mainComputerImg.textContent = "✊";
+        break;
+      case "paper":
+        mainComputerImg.textContent = "✋";
+        break;
+      case "scissors":
+        mainComputerImg.textContent = "✌";
+        break;
+      default:
+        alert("Something is wrong.");
+    }
+
+    mainHumanText.textContent = `Player: ${humanScore}`;
+    mainComputerText.textContent = `Computer: ${computerScore}`;
   }
-  console.log(
-    `Human Score: ${humanScore} \t Computer Score: ${computerScore}`
-  );
 }
 
-function checkGameOver (roundNumber) {
-    if (roundNumber >= totalRounds) {
-        return true;
-    } 
-    return false;
+function checkGameOver(roundNumber) {
+  if (humanScore >= 5 || computerScore >= 5) {
+    return true;
+  }
+  return false;
 }
 
-function getGameWinner () {
-    if (humanScore > computerScore) {
-        return `Human`;
-    }
-    else if (computerScore > humanScore) {
-        return `Computer`;
-    }
-    else {
-        return `Both human and computer have`;
-    }
+function getGameWinner() {
+  if (humanScore > computerScore) {
+    return `Human`;
+  } else if (computerScore > humanScore) {
+    return `Computer`;
+  }
+}
+
+function showModal(winner) {
+  let overlay = document.querySelector(".overlay");
+  let modalText = document.querySelector(".modal-text");
+  let modalButton = document.querySelector(".modal-button");
+
+  if (winner == "Human") {
+    modalText.style.background = "#00ff1599";
+  } else if (winner == "Computer") {
+    modalText.style.background = "#ff000099";
+  }
+
+  modalText.textContent = `${winner} won !!`;
+
+  if (overlay.style.visibility == "") {
+    overlay.style.visibility = "visible";
+  } else {
+    overlay.style.visibility = "";
+  }
+
+  modalButton.addEventListener("click", (e)=> {
+    showModal();
+    resetAll();
+  })
+}
+
+function resetAll () {
+  computerScore = 0,
+  humanScore = 0;
+  let mainText = document.querySelector(".main-text");
+  let subText = document.querySelector(".main-sub-text");
+  let mainHumanImg = document.querySelector(".main-human-img");
+  let mainComputerImg = document.querySelector(".main-computer-img");
+  let mainHumanText = document.querySelector(".main-human-text");
+  let mainComputerText = document.querySelector(".main-computer-text");
+
+  mainText.textContent = "Choose a weapon !!";
+  subText.textContent = "First to score 5 points wins the game.";
+  mainHumanImg.textContent = "?";
+  mainComputerImg.textContent = "?";
+  mainHumanText.textContent = "Player: 0";
+  mainComputerText.textContent = "Computer: 0";
 }
